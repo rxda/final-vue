e5a632c2777c5675cf8e6fad85c8319a965dced7
sxfscool <sxfscool@gmail.com>
2025-02-27 04:52:01 +0000

    init

---
+++ src/components/Car.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+    let car = ref({brand:'奔驰', price: 40})
+
+    let depreciate = () => {
+        car.value.price-=1;
+    }
+
+</script>
+
+<template>
+    <div>
+            <h2>
+                汽车信息： 一辆 {{ car.brand }}, 价值： {{ car.price }} 万
+            </h2>
+            <button @click="depreciate">贬值</button>
+    </div>
+
+
+</template>
