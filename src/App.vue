774af8be11e4346581104b107bb1786b896b39cf
sxfscool <sxfscool@gmail.com>
2025-03-06 01:29:18 +0000

    feat: dep

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,14 @@ import TagRef from './components/5.TagRef.vue';
 import PersonList from './components/6.Pros/PersonList.vue';
 import LifeCycle from './components/7.LifeCycle.vue';
 import Cat from './components/8.hooks/Cat.vue';
+import Router from './components/9.routes/1.Router.vue';
+import State from './components/10.stateManage/State.vue';
 </script>
 
 
 <template>
-  <Cat/>
+  <State/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from './components/9.routes/router'
 
-createApp(App).mount('#app')
+
+createApp(App).use(router).use(createPinia()).mount('#app')
